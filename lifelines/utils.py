a690ad61778712b2bb72a6b6229ed89aa21e7a2f
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2013-10-13 11:10:40 -0400

    changing return on datetimes_to_durations, updating docs with same fucion

--- lifelines/utils.py
+++ lifelines/utils.py
@@ -7,9 +7,17 @@ from pandas import to_datetime
 import pdb
 
 def dataframe_from_events_censorship(event_times, censorship):
-    """Accepts:
+    """
+    Parameters:
         event_times: (n,1) array of event times 
         censorship: if not None, (n,1) boolean array, 1 if observed event, 0 is censored
+
+    Returns:
+        Pandas DataFrame with index as the unique times in event_times. The columns named 
+        'removed' refers to the number of individuals who were removed from the population 
+        by the end of the period. The column 'observed' refers to the number of removed 
+        individuals who were observed to have died (i.e. not censored.)
+
     """
     df = pd.DataFrame( event_times, columns=["event_at"] )
     df["removed"] = 1
@@ -57,7 +65,7 @@ def datetimes_to_durations( start_times, end_times, fill_date = None, freq='D', 
 
     T = (end_times_ - start_times_).map(lambda x: x.astype(freq_string).astype(float) )
 
-    return T,C
+    return T.values, C.values
 
 
 def kernel_smoother(timeline, hazards, sigma):
