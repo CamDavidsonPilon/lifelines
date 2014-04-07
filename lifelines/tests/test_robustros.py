ef5c8f4587b22edfc4e3f2713ec14251e0dd8a9e
Paul Hobson <pmhobson@gmail.com>
2014-04-06 20:53:25 -0700

    updated ROS tests and moved away from nose

--- lifelines/tests/test_robustros.py
+++ lifelines/tests/test_robustros.py
@@ -1,39 +1,36 @@
-import os
+import unittest
 
-from nose.tools import *
 import numpy as np
-import numpy.testing as nptest
+import numpy.testing as npt
 
 import pandas as pd
 
-from ..estimation import RobustROS
+from ..estimation import RobustROSFitter
 
 
-class _base_ROS_Mixin:
+class _base_ROS_Mixin(unittest.TestCase):
 
-    @raises(ValueError)
     def test_zero_data(self):
         data = self.data.copy()
         data['res'].loc[0] = 0.0
-        ros = RobustROS(data)
+        npt.assert_raises(ValueError, RobustROSFitter, data)
 
-    @raises(ValueError)
     def test_negative_data(self):
         data = self.data.copy()
         data['res'].loc[0] = -1.0
-        ros = RobustROS(data)
+        npt.assert_raises(ValueError, RobustROSFitter, data)
 
     def test_data(self):
-        assert_true(hasattr(self.ros, 'data'))
-        assert_is_instance(self.ros.data, pd.DataFrame)
+        self.assertTrue(hasattr(self.ros, 'data'))
+        self.assertTrue(isinstance(self.ros.data, pd.DataFrame))
 
     def test_data_cols(self):
         known_cols = ['modeled', 'res', 'cen']
-        assert_list_equal(self.ros.data.columns.tolist(), known_cols)
+        npt.assert_array_equal(self.ros.data.columns.tolist(), known_cols)
 
     def test_debug_attr(self):
-        assert_true(hasattr(self.ros, 'debug'))
-        assert_is_instance(self.ros.data, pd.DataFrame)
+        self.assertTrue(hasattr(self.ros, 'debug'))
+        self.assertTrue(isinstance(self.ros.data, pd.DataFrame))
 
     def test_debug_cols(self):
         known_cols = [
@@ -41,34 +38,34 @@ class _base_ROS_Mixin:
             'Rank', 'plot_pos', 'Zprelim',
             'modeled_data', 'modeled'
         ]
-        assert_list_equal(self.ros.debug.columns.tolist(), known_cols)
+        npt.assert_array_equal(self.ros.debug.columns.tolist(), known_cols)
 
     def test_N_obs(self):
-        assert_true(hasattr(self.ros, 'N_obs'))
-        assert_equal(self.ros.N_obs, self.data.shape[0])
+        self.assertTrue(hasattr(self.ros, 'N_obs'))
+        npt.assert_equal(self.ros.N_obs, self.data.shape[0])
 
     def test_N_cen(self):
-        assert_true(hasattr(self.ros, 'N_cen'))
-        assert_equal(self.ros.N_cen, self.data[self.data.cen].shape[0])
+        self.assertTrue(hasattr(self.ros, 'N_cen'))
+        npt.assert_equal(self.ros.N_cen, self.data[self.data.cen].shape[0])
 
     def test_cohn_attr(self):
-        assert_true(hasattr(self.ros, 'cohn'))
-        assert_equal(type(self.ros.cohn), pd.DataFrame)
+        self.assertTrue(hasattr(self.ros, 'cohn'))
+        self.assertTrue(isinstance(self.ros.cohn, pd.DataFrame))
 
     def test_cohn_A(self):
-        nptest.assert_array_equal(self.known_cohn_A,
+        npt.assert_array_equal(self.known_cohn_A,
                                   self.ros.cohn['A'].values)
 
     def test_cohn_B(self):
-        nptest.assert_array_equal(self.known_cohn_B,
+        npt.assert_array_equal(self.known_cohn_B,
                                   self.ros.cohn['B'].values)
 
     def test_cohn_C(self):
-        nptest.assert_array_equal(self.known_cohn_C,
+        npt.assert_array_equal(self.known_cohn_C,
                                   self.ros.cohn['C'].values)
 
     def test_cohn_PE(self):
-        nptest.assert_array_almost_equal(
+        npt.assert_array_almost_equal(
             self.known_cohn_PE, self.ros.cohn['PE'], decimal=4
         )
 
@@ -84,17 +81,15 @@ class _base_ROS_Mixin:
         fdarray = np.array(self.ros.data.modeled)
         fdarray.sort()
 
-        nptest.assert_array_almost_equal(known_fd, fdarray, decimal=2)
+        npt.assert_array_almost_equal(known_fd, fdarray, decimal=2)
 
-    @raises(ValueError)
     def test_dup_index_error(self):
         data = self.data.append(self.data)
-        ros = RobustROS(data)
+        npt.assert_raises(ValueError, RobustROSFitter, data)
 
-    @raises(ValueError)
     def test_non_dataframe_error(self):
         data = self.data.values
-        ros = RobustROS(data)
+        npt.assert_raises(ValueError, RobustROSFitter, data)
 
 
 class test_ROS_Helsel_Arsenic(_base_ROS_Mixin):
@@ -102,7 +97,7 @@ class test_ROS_Helsel_Arsenic(_base_ROS_Mixin):
     Oahu arsenic data from Nondetects and Data Analysis by Dennis R. Helsel
     (John Wiley, 2005)
     '''
-    def setup(self):
+    def setUp(self):
         obs = np.array([3.2, 2.8, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
                         2.0, 2.0, 1.7, 1.5, 1.0, 1.0, 1.0, 1.0,
                         0.9, 0.9, 0.7, 0.7, 0.6, 0.5, 0.5, 0.5])
@@ -115,7 +110,8 @@ class test_ROS_Helsel_Arsenic(_base_ROS_Mixin):
         self.known_cohn_C = np.array([0.0, 1.0, 4.0, 8.0, np.nan])
         self.known_cohn_PE = np.array([1.0, 0.3125, 0.2143, 0.0833, 0.0])
         self.data = pd.DataFrame({'res': obs, 'cen': cen})
-        self.ros = RobustROS(self.data, rescol='res', cencol='cen')
+        self.ros = RobustROSFitter(self.data, result_col='res',
+                                   censorship_col='cen')
         self.ros.fit()
 
 
