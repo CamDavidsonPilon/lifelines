2730ccc95b0c52d90b6acbd49d5f78b0cb986864
Paul Hobson <pmhobson@gmail.com>
2014-04-08 10:24:14 -0700

    added another RobustROSFitter test dataset

--- lifelines/tests/test_robustros.py
+++ lifelines/tests/test_robustros.py
@@ -42,6 +42,10 @@ class _base_ROS_Mixin(unittest.TestCase):
         ]
         npt.assert_array_equal(self.ros.debug.columns.tolist(), known_cols)
 
+    def test_plotting_positions(self):
+        pp = np.round(np.array(self.ros.debug.plot_pos), 3)
+        npt.assert_array_almost_equal(pp, self.known_plot_pos, decimal=3)
+
     def test_N_obs(self):
         self.assertTrue(hasattr(self.ros, 'N_obs'))
         npt.assert_equal(self.ros.N_obs, self.data.shape[0])
@@ -72,18 +76,10 @@ class _base_ROS_Mixin(unittest.TestCase):
         )
 
     def test_MR_rosEstimator(self):
-        known_fd = np.array([
-            3.20, 2.80, 1.42, 1.14, 0.95, 0.81,
-            0.68, 0.57, 0.46, 0.35, 1.70, 1.50,
-            0.98, 0.76, 0.58, 0.41, 0.90, 0.61,
-            0.70, 0.70, 0.60, 0.50, 0.50, 0.50
-        ])
-        known_fd.sort()
+        modeled = np.array(self.ros.data.modeled)
+        modeled.sort()
 
-        fdarray = np.array(self.ros.data.modeled)
-        fdarray.sort()
-
-        npt.assert_array_almost_equal(known_fd, fdarray, decimal=2)
+        npt.assert_array_almost_equal(self.known_modeled, modeled, decimal=2)
 
     def test_dup_index_error(self):
         data = self.data.append(self.data)
@@ -119,10 +115,52 @@ class test_ROS_Helsel_Arsenic(_base_ROS_Mixin):
         self.known_cohn_B = np.array([0.0, 7.0, 12.0, 22.0, np.nan])
         self.known_cohn_C = np.array([0.0, 1.0, 4.0, 8.0, np.nan])
         self.known_cohn_PE = np.array([1.0, 0.3125, 0.2143, 0.0833, 0.0])
+        self.known_plot_pos = np.array([
+            0.102,  0.157,  0.204,  0.306,  0.314,  0.344,  0.407,  0.471,
+            0.509,  0.611,  0.629,  0.713,  0.815,  0.098,  0.196,  0.295,
+            0.393,  0.491,  0.589,  0.737,  0.829,  0.873,  0.944,  0.972
+        ])
+        self.known_modeled = np.array([
+            3.20, 2.80, 1.42, 1.14, 0.95, 0.81, 0.68, 0.57, 0.46, 0.35, 1.70, 1.50,
+            0.98, 0.76, 0.58, 0.41, 0.90, 0.61, 0.70, 0.70, 0.60, 0.50, 0.50, 0.50
+        ])
+        self.known_modeled.sort()
         self.data = pd.DataFrame({'res': obs, 'cen': cen})
         self.ros = RobustROSFitter(self.data, result_col='res',
                                    censorship_col='cen')
         self.ros.fit()
+
+class test_ROS_Helsel_AppendixB(_base_ROS_Mixin):
+    '''
+    Appendix B dataset from "Estimation of Descriptive Statists for Multiply
+    Censorsed Water Quality Data", Water Resources Research, Vol 24,
+    No 12, pp 1997 - 2004. December 1988.
+    '''
+    def setUp(self):
+        obs = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 10., 10., 10.,
+                        3.0, 7.0, 9.0, 12., 15., 20., 27., 33., 50.])
+        cen = np.array([True, True, True, True, True, True, True, True, True,
+                        False, False, False, False, False, False, False,
+                        False, False])
+
+        self.known_cohn_A = np.array([3.0, 6.0, np.nan])
+        self.known_cohn_B = np.array([6.0, 12.0, np.nan])
+        self.known_cohn_C = np.array([6.0, 3.0, np.nan])
+        self.known_cohn_PE = np.array([0.5555, 0.3333, 0.0])
+        self.known_plot_pos = np.array([
+            0.063, 0.127, 0.167, 0.190, 0.254, 0.317, 0.333, 0.381, 0.500,
+            0.500, 0.556, 0.611, 0.714, 0.762, 0.810, 0.857, 0.905, 0.952
+        ])
+        self.known_modeled = np.array([
+            0.47,  0.85, 1.11, 1.27, 1.76, 2.34, 2.50, 3.00, 3.03,
+            4.80, 7.00, 9.00, 12.0, 15.0, 20.0, 27.0, 33.0, 50.0
+        ])
+        self.known_modeled.sort()
+        self.data = pd.DataFrame({'res': obs, 'cen': cen})
+        self.ros = RobustROSFitter(self.data, result_col='res',
+                                   censorship_col='cen')
+        self.ros.fit()
+
 
 
 
