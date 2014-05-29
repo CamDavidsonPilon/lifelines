21c37a9a5d2b8c68cd4adcdd2d04b6e80aad1041
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2014-05-28 23:25:13 -0400

    bumpnig

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="lifelines",
-    version="0.3.2.1",
+    version="0.3.2.2",
     author="Cameron Davidson-Pilon",
     author_email="cam.davidson.pilon@gmail.com",
     description="Survival analysis in Python, including Kaplan Meier, Nelson Aalen and regression",
