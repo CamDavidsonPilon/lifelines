cf8b180a2e9a0dce844e8412f23f6c99e8fa3ece
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-03-28 09:46:29 -0400

    making lifelines datasets easier to import. See #140

--- lifelines/__init__.py
+++ lifelines/__init__.py
@@ -3,6 +3,8 @@ from .estimation import KaplanMeierFitter, NelsonAalenFitter, \
     AalenAdditiveFitter, BreslowFlemingHarringtonFitter, CoxPHFitter, \
     WeibullFitter, ExponentialFitter
 
+import datasets
+
 __all__ = ['KaplanMeierFitter', 'NelsonAalenFitter', 'AalenAdditiveFitter',
            'BreslowFlemingHarringtonFitter', 'CoxPHFitter', 'WeibullFitter',
            'ExponentialFitter']
