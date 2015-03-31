b6b2421b232eb4c9d83410e8012e6091a3f2c846
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-03-30 21:05:03 -0400

    fixing travis error

--- lifelines/__init__.py
+++ lifelines/__init__.py
@@ -3,7 +3,7 @@ from .estimation import KaplanMeierFitter, NelsonAalenFitter, \
     AalenAdditiveFitter, BreslowFlemingHarringtonFitter, CoxPHFitter, \
     WeibullFitter, ExponentialFitter
 
-import datasets
+import .datasets
 
 __all__ = ['KaplanMeierFitter', 'NelsonAalenFitter', 'AalenAdditiveFitter',
            'BreslowFlemingHarringtonFitter', 'CoxPHFitter', 'WeibullFitter',
