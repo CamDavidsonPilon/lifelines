07284381320f33c4081b159829378f4216f482da
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-05-28 22:40:04 -0400

    update

--- lifelines/datasets/__init__.py
+++ lifelines/datasets/__init__.py
@@ -22,14 +22,15 @@ def load_holly_molly_polly(**kwargs):
     From https://stat.ethz.ch/education/semesters/ss2011/seminar/contents/presentation_10.pdf
     Used as a toy example for CoxPH in recurrent SA. 
 
-    ID Status Stratum Start (days) Stop (days) tx
-    M 1 1 0 100 1
-    M 1 2 100 105 1
-    H 1 1 0 30 0
-    H 1 2 30 50 0
-    P 1 1 0 20 0
-    P 1 2 20 60 0
-    P 1 3 60 85 0
+     ID Status Stratum Start(days) Stop(days) tx T
+     M 1 1 0 100 1 100
+     M 1 2 100 105 1 5
+     H 1 1 0 30 0 30
+     H 1 2 30 50 0 20
+     P 1 1 0 20 0 20
+     P 1 2 20 60 0 40
+     P 1 3 60 85 0 25
+
     """
     return load_dataset('holly_molly_polly.tsv', sep="\s")
 
