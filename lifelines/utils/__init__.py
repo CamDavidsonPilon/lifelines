98ad81df5fbe696ca8e07b233914f496a85542eb
Niels Bantilan <niels@peggedsoftware.com>
2015-12-21 12:05:50 -0500

    Added a fitter_kwargs argument to k_fold_cross_validation utility function
    - when doing k_fold_cross_validation, the function should be able
    to pash in key-word arguments to the fitter
    - for example, for the coxph model, you should be able to pass
    in the strata argument

--- lifelines/utils/__init__.py
+++ lifelines/utils/__init__.py
@@ -416,7 +416,8 @@ def inv_normal_cdf(p):
 
 def k_fold_cross_validation(fitters, df, duration_col, event_col=None,
                             k=5, evaluation_measure=concordance_index,
-                            predictor="predict_median", predictor_kwargs={}):
+                            predictor="predict_median", fitter_kwargs={},
+                            predictor_kwargs={}):
     """
     Perform cross validation on a dataset. If multiple models are provided,
     all models will train on each of the k subsets.
@@ -482,7 +483,8 @@ def k_fold_cross_validation(fitters, df, duration_col, event_col=None,
 
         for fitter, scores in zip(fitters, fitterscores):
             # fit the fitter to the training data
-            fitter.fit(training_data, duration_col=duration_col, event_col=event_col)
+            fitter.fit(training_data, duration_col=duration_col,
+                       event_col=event_col, **fitter_kwargs)
             T_pred = getattr(fitter, predictor)(X_testing, **predictor_kwargs).values
 
             try:
