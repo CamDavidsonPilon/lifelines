7c7168ed4ea2d9587f09685e12ac767d4ccebfb7
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2017-07-19 22:41:44 -0400

    other doc changes

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # lifelines documentation build configuration file, created by
@@ -11,9 +12,6 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys
-import os
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -48,8 +46,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'lifelines'
-copyright = u'2014, Cam Davidson-Pilon'
+project = 'lifelines'
+copyright = '2014, Cam Davidson-Pilon'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,8 +199,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'lifelines.tex', u'lifelines Documentation',
-   u'Cam Davidson-Pilon', 'manual'),
+  ('index', 'lifelines.tex', 'lifelines Documentation',
+   'Cam Davidson-Pilon', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -231,8 +229,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'lifelines', u'lifelines Documentation',
-     [u'Cam Davidson-Pilon'], 1)
+    ('index', 'lifelines', 'lifelines Documentation',
+     ['Cam Davidson-Pilon'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -245,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'lifelines', u'lifelines Documentation',
-   u'Cam Davidson-Pilon', 'lifelines', 'Survival analysis in Python.'),
+  ('index', 'lifelines', 'lifelines Documentation',
+   'Cam Davidson-Pilon', 'lifelines', 'Survival analysis in Python.'),
 ]
 
 # Documents to append as an appendix to all manuals.
@@ -263,6 +261,7 @@ texinfo_documents = [
 
 # use RTFD theme locally
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
