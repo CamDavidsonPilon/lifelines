70a6f6c8def4d5ef9b7e472eedcd4dc90fd50b58
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2017-12-08 23:20:03 -0500

    classic ggg

--- tests/test_estimation.py
+++ tests/test_estimation.py
@@ -452,7 +452,7 @@ class TestKaplanMeierFitter():
 
         expected_upper_bound = np.array([0.975, 0.904, 0.804, 0.676])
         npt.assert_array_almost_equal(kmf.confidence_interval_['KM_estimate_upper_0.95'].values,
-                                      expected_upper_bound, decimal=3)g
+                                      expected_upper_bound, decimal=3)
 
     def test_kmf_does_not_drop_to_zero_if_last_point_is_censored(self):
         T = np.arange(0, 50, 0.5)
