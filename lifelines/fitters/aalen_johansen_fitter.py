51d3a8e5eda80e2aaa8e6312c9cd6fabeb51741a
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2018-05-01 18:37:43 -0400

    Update aalen_johansen_fitter.py
    Added UnivariateFitter to initial call
    Fixed internal function _jitter()
    Changed instances of len() with shape[]
    Removed inplace
    Updated names to be more clear
    Added reference to _bounds()

--- lifelines/fitters/aalen_johansen_fitter.py
+++ lifelines/fitters/aalen_johansen_fitter.py
@@ -2,41 +2,22 @@ from __future__ import print_function
 from __future__ import division
 import numpy as np
 import pandas as pd
+import warnings
 
-from lifelines.fitters import KaplanMeierFitter
+from lifelines.fitters import UnivariateFitter
 from lifelines.utils import _preprocess_inputs
+from lifelines.fitters.kaplan_meier_fitter import KaplanMeierFitter
 
-
-class AalenJohansenFitter():
+class AalenJohansenFitter(UnivariateFitter):
     """
     Class for fitting the Aalen-Johansen estimate for the cumulative incidence function in a competing 
     risks framework.
     
-    AalenJohansenFitter()
+    AalenJohansenFitter(alpha=0.95,jitter_level=0.00001,seed=None)
     
+    Aalen-Johansen cannot deal with tied times. We can get around this by randomy jittering the event times 
+    slightly. This will be done automatically and generate a warning.
     """
-    def jitter(event_time,jitter_level=0.00001,seed=None):
-        '''Aalen-Johansen cannot deal with tied times. We can get around this
-        by randomy jittering the event times slightly. 
-        
-        Parameters:
-            event_time: array or pd.Series with time of event/censoring
-            jitter_level: how much jittering to apply to data 
-        
-        Returns:
-            pd.Series of jittered times
-        '''
-        if seed != None:
-            np.random.seed(seed)
-        jf = pd.DataFrame()
-        jf['t'] = event_time
-        jf['mark'] = -1 * np.random.binomial(1,0.5,size=len(jf)) #half increase, half decrease
-        jf.loc[jf['mark']==0,'mark'] = 1 
-        jf['randon_uniform'] = np.random.uniform(size=len(df)) #random amount to jitter 
-        jf['t2'] = jf['t'] + (jf['mark']*jf['randon_uniform']*jitter_level) #jittering times
-        return jf['t2'] #return Pandas Series of jittered times
-
-    
     def fit(self,durations,event_observed,event_ind,timeline=None,entry=None,weights=None):
         """
         Parameters:
@@ -51,29 +32,62 @@ class AalenJohansenFitter():
              were born at time 0.
           weights: weights for event
         """
-        if np.sum(durations.duplicated()) > 0: #raise error if duplicated times
-            raise ValueError('Aalen-Johansen cannot be estimated with tied events')
-        cmprisk_label = 'CDF_'+str(int(event_ind))
+        #First, we check to see if tied event times, if so then need to randomly jitter the data
+        if np.sum(durations.duplicated()) > 0: #raise warning if duplicated times, then randomly jitter times
+            warnings.warn('''Tied event times were detected. The Aalen-Johansen estimator cannot handle tied event times. 
+                To resolve ties, data is randomly jittered.''',Warning)
+            durations = _jitter(event_time=durations)
+        
+        #Setting competing risk label, it is based on the event indicator specified
+        cmprisk_label = 'CIF_'+str(int(event_ind))
         overall_events = np.where(event_observed>0,1,0) #create indicator for any event occurring
-        km = KaplanMeierFitter().fit(durations,event_observed=overall_events,timeline=timeline,entry=entry) #Fit KM for survival times
-        aj = km.event_table #extract out KM info
-        aj['Overall_S'] = km.survival_function_
-        event_spec = np.where(event_observed==event_ind,1,0) #extract observed event counts for each unique event
+        
+        #Fit overall Kaplan Meier for any event types
+        km = KaplanMeierFitter().fit(durations,event_observed=overall_events,timeline=timeline,entry=entry)
+        aj = km.event_table
+        aj['overall_survival'] = km.survival_function_
+        aj['lagged_overall_survival'] = aj['overall_survival'].shift()
+        
+        #Determine discrete time hazards for event type specified by event_ind
+        event_spec = np.where(event_observed==event_ind,1,0)
         event_spec_proc = _preprocess_inputs(durations=durations,event_observed=event_spec,timeline=timeline,entry=entry,weights=weights)
         event_spec_times = event_spec_proc[-1]['observed']
         label_cmprisk = 'observed_'+str(int(event_ind))
-        event_spec_times.rename(label_cmprisk,inplace=True)
+        event_spec_times = event_spec_times.rename(label_cmprisk)
         aj = pd.concat([aj,event_spec_times],axis=1)
-        aj['Overall_S_lag'] = aj['Overall_S'].shift() #creating lagged survival times
-        aj.reset_index(inplace=True)
-        aj['h-h'] = -np.log(aj['Overall_S']) + np.log(aj['Overall_S_lag']) #calculating discrete time hazard
+        aj = aj.reset_index().copy()        
+        aj['discrete_hazard'] = -np.log(aj['overall_survival']) + np.log(aj['lagged_overall_survival'])
         aj.loc[0,'h-h'] = 0 #setting initial discrete time hazard to zero
-        aj['stepsize'] = aj['Overall_S_lag'] * aj['h-h'] * aj[label_cmprisk]
-        aj[cmprisk_label] = aj['stepsize'].cumsum()
-        aj.loc[0,cmprisk_label] = 0
-        aj.set_index('event_at',inplace=True) 
-        self.event_table = aj[['removed','observed',cmprisk_label,'censored','entrance','at_risk']] #setting self items
-        self.cumulative_density_ = aj[cmprisk_label]
-        return self 
+        aj['stepsize'] = aj['lagged_overall_survival'] * aj['discrete_hazard'] * aj[label_cmprisk] #Calculating jump size
+        aj[cmprisk_label] = aj['stepsize'].cumsum() #Calculating Cumulative Incidence Function
+        aj.loc[0,cmprisk_label] = 0 #Setting initial CIF to be zero
+        aj = aj.set_index('event_at')
+        
+        #Setting attributes
+        self.event_table = aj[['removed','observed',label_cmprisk,'censored','entrance','at_risk']] #Event table, like KaplanMeier
+        self.cumulative_density_ = aj[cmprisk_label] #Technically, cumulative incidence, but labeled as density to keep consistency with Kaplan Meier
+        return self
+    
+    def _jitter(self,event_time,jitter_level=0.00001,seed=None):
+        '''Determine extent to jitter tied event times. Automatically called by fit if tied event times are detected
+        '''
+        #Setting inital seed
+        if seed != None:
+            np.random.seed(seed)
+            
+        #Determining whether to randomly shift event times up or down
+        mark = np.random.choice([-1,1],size=event_time.shape[0])
+        
+        #Determining extent to jitter event times up or down
+        shift = np.random.uniform(size=jf.shape[0])*jitter_level)
+        return event_time + mark*shift #Return randomly jittered event times
+
+    
+    def _bounds(self,alpha):
+        '''To be added...  I need to try and find an exponential version, so it matches the Kaplan Meier.
+        Currently, I only have a formula (based on Greenwoods) on pg411 of "Modelling Survival Data in Medical 
+        Research" David Collett 3rd Edition'''
+        return aj
+        
 
 
