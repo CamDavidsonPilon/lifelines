4db34117c6e15a623e84862f841b865e47e408e5
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2018-11-14 15:27:51 -0500

    Added plot and minor tweaks
    Verified the plot functionality with AalenJohansenFitter. Added to the documentation. Cleaned up the code a bit. 
    
    For the future, might consider adding the Aalen confidence interval method. Currently uses the Delta method. Not a big difference between the two. Both are in the SAS document I linked in the _bounds docs

--- lifelines/fitters/aalen_johansen_fitter.py
+++ lifelines/fitters/aalen_johansen_fitter.py
@@ -10,14 +10,23 @@ from lifelines.fitters.kaplan_meier_fitter import KaplanMeierFitter
 
 class AalenJohansenFitter(UnivariateFitter):
     """
-    Class for fitting the Aalen-Johansen estimate for the cumulative incidence function in a competing 
-    risks framework.
+    Class for fitting the Aalen-Johansen estimate for the cumulative incidence function in a competing risks framework.
+    Treating competing risks as censoring can result in over-estimated cumulative density functions. Using the Kaplan
+    Meier estimator with competing risks as censored is akin to estimating the cumulative density if all competing risks
+    had been prevented. If you are interested in learning more, I (Paul Zivich) recommend the following open-access
+    paper; Edwards JK, Hester LL, Gokhale M, Lesko CR. Methodologic Issues When Estimating Risks in
+    Pharmacoepidemiology. Curr Epidemiol Rep. 2016;3(4):285-296.
     
     AalenJohansenFitter(alpha=0.95,jitter_level=0.00001,seed=None)
     
     Aalen-Johansen cannot deal with tied times. We can get around this by randomy jittering the event times 
     slightly. This will be done automatically and generate a warning.
     """
+    def __init__(self, jitter_level=0.0001, seed=None, alpha=0.95):
+        UnivariateFitter.__init__(self, alpha=alpha)
+        self._jitter_level = jitter_level
+        self._seed = seed  # Seed is for the jittering process
+
     def fit(self,durations, event_observed, event_indicator, timeline=None, entry=None, label='AJ_estimate', 
             alpha=None, ci_labels=None, weights=None):
         """
@@ -43,54 +52,58 @@ class AalenJohansenFitter(UnivariateFitter):
 
         Returns:
           self, with new properties like 'cumulative_incidence_'.
-        
         """
-        #First, we check to see if tied event times, if so then need to randomly jitter the data
-        event_times = durations.loc[event_observed!=0].copy() #Only care about tied event times
-        if np.sum(durations.duplicated()) > 0: #raise warning if duplicated times, then randomly jitter times
+        # Checking for tied event times
+        if np.sum(durations.duplicated()) > 0:
+            # Seeing if there is a large amount of ties in the data (>20%)
+            if np.sum(durations.duplicated()) / len(durations) > 0.2:
+                warnings.warn('''It looks like there are many tied events in your data set. The Aalen-Johansen 
+                              estimator should only be used when there are no/few tied events''', Warning)
+            # Raise warning if duplicated times, then randomly jitter times
             warnings.warn('''Tied event times were detected. The Aalen-Johansen estimator cannot handle tied event times. 
                 To resolve ties, data is randomly jittered.''',Warning)  
-            durations = self._jitter(durations=durations,event=event_observed)
+            durations = self._jitter(durations=durations, event=event_observed, jitter_level=self._jitter_level)
         
-        #Setting competing risk label, it is based on the event indicator specified
+        # Creating label for event of interest & indicator for that event
         cmprisk_label = 'CIF_'+str(int(event_indicator))
-        overall_events = np.where(event_observed>0,1,0) #create indicator for any event occurring
+        self.label_cmprisk = 'observed_' + str(int(event_indicator))
+        overall_events = np.where(event_observed>0,1,0)
         
-        #Fit overall Kaplan Meier for any event types
-        km = KaplanMeierFitter().fit(durations,event_observed=overall_events,timeline=timeline,entry=entry,weights=weights)
+        # Fitting Kaplan-Meier for either event of interest OR competing risk
+        km = KaplanMeierFitter()  # TODO this uses as exploit in that KM does not check that event indicator is 0/1
+        km.fit(durations, event_observed=overall_events, timeline=timeline, entry=entry, weights=weights)
         aj = km.event_table
         aj['overall_survival'] = km.survival_function_
         aj['lagged_overall_survival'] = aj['overall_survival'].shift()
         
-        #Determine discrete time hazards for event type specified by event_ind
+        # Setting up table for calculations and to return to user
         event_spec = np.where(event_observed==event_indicator,1,0)
-        event_spec_proc = _preprocess_inputs(durations=durations,event_observed=event_spec,timeline=timeline,entry=entry,weights=weights)
+        event_spec_proc = _preprocess_inputs(durations=durations, event_observed=event_spec, timeline=timeline,
+                                             entry=entry,weights=weights)
         event_spec_times = event_spec_proc[-1]['observed']
-        self.label_cmprisk = 'observed_'+str(int(event_indicator))
         event_spec_times = event_spec_times.rename(self.label_cmprisk)
-        aj = pd.concat([aj,event_spec_times],axis=1)
-        aj = aj.reset_index().copy()        
-        aj['discrete_hazard'] = (aj[self.label_cmprisk]) / (aj['at_risk'])
-        aj['stepsize'] = aj['lagged_overall_survival'] * aj['discrete_hazard'] * aj[self.label_cmprisk] #Calculating jump size
-        aj[cmprisk_label] = aj['stepsize'].cumsum() #Calculating Cumulative Incidence Function
-        aj.loc[0,cmprisk_label] = 0 #Setting initial CIF to be zero
+        aj = pd.concat([aj,event_spec_times],axis=1).reset_index()
+
+        # Estimator of Cumulative Incidence (Density) Function
+        aj[cmprisk_label] = ((aj[self.label_cmprisk]) / (aj['at_risk']) * aj['lagged_overall_survival']).cumsum()
+        aj.loc[0,cmprisk_label] = 0  # Setting initial CIF to be zero
         aj = aj.set_index('event_at')
         
-        #Setting attributes
+        # Setting attributes
         alpha = alpha if alpha else self.alpha
         self._label = label
-        self.event_table = aj[['removed','observed',self.label_cmprisk,'censored','entrance','at_risk']] #Event table, like KaplanMeier
-        self.cumulative_density_ = aj[cmprisk_label] #Technically, cumulative incidence, but labeled as density to keep consistency with Kaplan Meier
-        self.variance, self.confidence_interval_ = self._bounds(aj['lagged_overall_survival'],alpha=alpha,ci_labels=ci_labels)
+        self.cumulative_density_ = aj[cmprisk_label] #Technically, cumulative incidence, but consistent with KaplanMeierFitter
+        self.event_table = aj[['removed','observed',self.label_cmprisk,'censored','entrance','at_risk']] #Event table
+        self.variance, self.confidence_interval_ = self._bounds(aj['lagged_overall_survival'],
+                                                                alpha=alpha, ci_labels=ci_labels)
+        self.plot = self._plot_estimate('cumulative_density_')
         return self
     
-    def _jitter(self,durations,event,jitter_level=0.00001,seed=None):
-        '''Determine extent to jitter tied event times. Automatically called by fit if tied event times are detected
-        '''
-        #Setting inital seed
+    def _jitter(self, durations, event, jitter_level, seed=None):
+        """Determine extent to jitter tied event times. Automatically called by fit if tied event times are detected
+        """
         if jitter_level <= 0:
             raise ValueError('The jitter level is less than zero, please select a jitter value greater than 0')
-
         if seed != None:
             np.random.seed(seed)
         
@@ -109,41 +122,45 @@ class AalenJohansenFitter(UnivariateFitter):
         else:
             return durations_jitter
     
-    def _bounds(self,lagged_survival,alpha,ci_labels):
-        '''To be added...  I need to try and find an exponential version, so it matches the Kaplan Meier.
-        Currently, I only have a formula (based on Greenwoods) on pg411 of "Modelling Survival Data in Medical 
-        Research" David Collett 3rd Edition
+    def _bounds(self,lagged_survival, alpha, ci_labels):
+        """Bounds are based on pg411 of "Modelling Survival Data in Medical Research" David Collett 3rd Edition, which
+        is derived from Greenwood's variance estimator. Confidence intervals are obtained using the delta method
+        transformation of SE(log(-log(F_j))). This ensures that the confidence intervals all lie between 0 and 1.
         
-        Var(F_j) = sum((F_j(t) - F_j(t_i))**2 * d/(n*(n-d) + S(t_i-1)**2 * ((d*(n-d))/n**3) + 
+        Formula for the variance follows:
+        Var(F_j) = sum((F_j(t) - F_j(t_i))**2 * d/(n*(n-d) + S(t_i-1)**2 * ((d*(n-d))/n**3) +
                     -2 * sum((F_j(t) - F_j(t_i)) * S(t_i-1) * (d/n**2)
-        
-        Confidence intervals are obtained using the delta method transformation of SE(log(-log(F_j))). This ensures
-        that the confidence intervals all lie between 0 and 1. 
-        
+
+        Delta method transformation:
         SE(log(-log(F_j) = SE(F_j) / (F_j * absolute(log(F_j)))
         
-        '''
+        More information can be found at: https://support.sas.com/documentation/onlinedoc/stat/141/lifetest.pdf
+        There is also an alternative method (Aalen) but this is not currently implemented
+        """
+        # Preparing environment
         df = self.event_table.copy()
         df['Ft'] = self.cumulative_density_
         df['lagS'] = lagged_survival.fillna(1)
-        all_vars = []
-        for i,r in df.iterrows():
-            sf = df.loc[df.index<=r.name].copy()
-            sf['part1'] = ((r['Ft'] - sf['Ft'])**2) * (sf['observed'] / (sf['at_risk']*(sf['at_risk'] - sf['observed'])))
-            sf['part2'] = ((sf['lagS'])**2) * ((sf[self.label_cmprisk] * ((sf['at_risk']-sf[self.label_cmprisk])/(sf['at_risk']**3))))
-            sf['part3'] = (r['Ft'] - sf['Ft']) * sf['lagS'] * (sf[self.label_cmprisk] / (sf['at_risk']**2))
-            variance = (sf['part1'].sum()) + (sf['part2'].sum()) - 2*(sf['part3'].sum())
-            all_vars.append(variance)
-        df['variance'] = all_vars
-        
         if ci_labels is None:
             ci_labels = ["%s_upper_%.2f" % (self._label, alpha), "%s_lower_%.2f" % (self._label, alpha)]
         assert len(ci_labels) == 2, "ci_labels should be a length 2 array."
+
+        # Have to loop through each time independently. Don't think there is a faster way
+        all_vars = []
+        for i,r in df.iterrows():
+            sf = df.loc[df.index<=r.name].copy()
+            F_t = float(r['Ft'])
+            sf['part1'] = ((F_t - sf['Ft'])**2) * (sf['observed'] / (sf['at_risk']*(sf['at_risk'] - sf['observed'])))
+            sf['part2'] = ((sf['lagS'])**2) * sf[self.label_cmprisk] * ((sf['at_risk']-
+                                                                         sf[self.label_cmprisk]))/(sf['at_risk']**3)
+            sf['part3'] = (F_t - sf['Ft']) * sf['lagS'] * (sf[self.label_cmprisk] / (sf['at_risk']**2))
+            variance = (np.sum(sf['part1'])) + (np.sum(sf['part2'])) - 2*(np.sum(sf['part3']))
+            all_vars.append(variance)
+        df['variance'] = all_vars        
         
         #Calculating Confidence Intervals
         df['F_transformed'] = np.log(-np.log(df['Ft']))
-        df['se'] = np.sqrt(df['variance'])
-        df['se_transformed'] = df['se'] / (df['Ft'] * np.absolute(np.log(df['Ft'])))
+        df['se_transformed'] = np.sqrt(df['variance']) / (df['Ft'] * np.absolute(np.log(df['Ft'])))
         zalpha = inv_normal_cdf((1. + alpha) / 2.)
         df[ci_labels[0]] = np.exp(-np.exp(df['F_transformed']+zalpha*df['se_transformed']))
         df[ci_labels[1]] = np.exp(-np.exp(df['F_transformed']-zalpha*df['se_transformed']))
