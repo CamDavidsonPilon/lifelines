7f9a7ca0ae9b398984ca80557183aea1a3136281
Cam Davidson-Pilon <cam.davidson.pilon@gmail.com>
2019-01-03 06:18:09 -0500

    wip

--- perf_tests/cp_perf_test.py
+++ perf_tests/cp_perf_test.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     from lifelines.datasets import load_rossi
 
     df = load_rossi()
-    df = pd.concat([df] * 10)
+    df = pd.concat([df] * 40)
     # df['week'] = np.random.exponential(1, size=df.shape[0])
     cp = CoxPHFitter()
     start_time = time.time()
-    cp.fit(df, duration_col="week", event_col="arrest")
+    cp.fit(df, duration_col="week", event_col="arrest", batch_mode=True)
     print("--- %s seconds ---" % (time.time() - start_time))
     cp.print_summary()
