88c6c00fe6b2cb4cb26b3f1aae93455410ccda8a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2019-02-22 12:14:01 -0500

    fix tests

--- lifelines/utils/__init__.py
+++ lifelines/utils/__init__.py
@@ -805,7 +805,7 @@ def pass_for_numeric_dtypes_or_raise_array(x):
 
     """
     try:
-        return pd.to_numeric(x.squeeze())
+        return pd.to_numeric(np.asarray(x).squeeze())
     except:
         raise ValueError("Values must be numeric: no strings, datetimes, objects, etc.")
 
