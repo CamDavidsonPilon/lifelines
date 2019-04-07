fixing loglikelihood ratio test #687 and some good perf improvements there too

lifelines/fitters/coxph_fitter.py
@@ -451,6 +451,12 @@ estimate the variances. See paper "Variance estimation when using inverse probab
                     h += _h
                     ll += _ll
 
+            if i == 1 and np.all(beta == 0):
+                # this is a neat optimization, the null partial likelihood
+                # is the same as the full partial but evaluated at zero.
+                # if the user supplied a non-trivial initial point, we need to delay this.
+                self._log_likelihood_null = ll
+
             if self.penalizer > 0:
                 # add the gradient and hessian of the l2 term
                 g -= self.penalizer * beta
@@ -1278,16 +1284,18 @@ See https://stats.stackexchange.com/questions/11109/how-to-deal-with-perfect-sep
         compare the existing model (with all the covariates) to the trivial model
         of no covariates.
         """
-        if self._batch_mode:
-            ll_null = self._trivial_log_likelihood_batch(
-                self.durations.values, self.event_observed.values, self.weights.values
-            )
+        if hasattr(self, "_log_likelihood_null"):
+            ll_null = self._log_likelihood_null
         else:
-            ll_null = self._trivial_log_likelihood_single(
-                self.durations.values, self.event_observed.values, self.weights.values
-            )
+            if self._batch_mode:
+                ll_null = self._trivial_log_likelihood_batch(
+                    self.durations.values, self.event_observed.values, self.weights.values
+                )
+            else:
+                ll_null = self._trivial_log_likelihood_single(
+                    self.durations.values, self.event_observed.values, self.weights.values
+                )
         ll_alt = self._log_likelihood
-
         test_stat = 2 * ll_alt - 2 * ll_null
         degrees_freedom = self.hazards_.shape[0]
         p_value = chisq_test(test_stat, degrees_freedom=degrees_freedom)

tests/test_estimation.py
@@ -3872,16 +3872,26 @@ Likelihood ratio test = 15.11 on 4 df, -log2(p)=7.80
         data(heart)
         r = coxph(Surv(start, stop, event) ~ age + strata(transplant) + surgery + year, data=heart)
         r
+        logLik(r)
         """
         ctv.fit(heart, id_col="id", event_col="event", strata="transplant")
         summary = ctv.summary.sort_index()
         npt.assert_allclose(summary["coef"].tolist(), [0.0293, -0.6176, -0.1527], atol=0.001)
         npt.assert_allclose(summary["se(coef)"].tolist(), [0.0139, 0.3707, 0.0710], atol=0.001)
         npt.assert_allclose(summary["z"].tolist(), [2.11, -1.67, -2.15], atol=0.01)
+        npt.assert_allclose(ctv._log_likelihood, -254.7144, atol=0.01)
 
     def test_ctv_with_multiple_strata(self, ctv, heart):
         ctv.fit(heart, id_col="id", event_col="event", strata=["transplant", "surgery"])
-        assert True
+        npt.assert_allclose(ctv._log_likelihood, -230.6726, atol=0.01)
+
+    def test_ctv_ratio_test_with_strata(self, ctv, heart):
+        ctv.fit(heart, id_col="id", event_col="event", strata=["transplant"])
+        npt.assert_allclose(ctv._compute_likelihood_ratio_test()[0], 15.68, atol=0.01)
+
+    def test_ctv_ratio_test_with_strata_and_initial_point(self, ctv, heart):
+        ctv.fit(heart, id_col="id", event_col="event", strata=["transplant"], initial_point=0.1 * np.ones(3))
+        npt.assert_allclose(ctv._compute_likelihood_ratio_test()[0], 15.68, atol=0.01)
 
 
 class TestAalenJohansenFitter: