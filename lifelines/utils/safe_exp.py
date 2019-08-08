399fccc04731bf5a24f7837e0b6df53c743915d4
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2019-08-07 21:57:33 -0400

    adding a new 'safe_exp' util

---
+++ lifelines/utils/safe_exp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+from autograd.extend import primitive, defvjp
+from autograd import numpy as np
+
+MAX = np.log(sys.float_info.max) - 50
+
+
+def safe_exp_vjp(ans, x):
+    return lambda g: g * ans
+
+
+@primitive
+def safe_exp(x):
+    return np.exp(np.clip(x, -np.inf, MAX))
+
+
+defvjp(safe_exp, safe_exp_vjp)
