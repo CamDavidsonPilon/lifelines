5c0b540051f0448d842008b41dc7bd26a158fbee
Sandu Ursu <sandu.ursu@gmail.com>
2019-08-25 19:47:49 +0200

    Changed .unique().shape[0] to .nunique()

--- lifelines/fitters/cox_time_varying_fitter.py
+++ lifelines/fitters/cox_time_varying_fitter.py
@@ -227,7 +227,7 @@ class CoxTimeVaryingFitter(BaseFitter):
         self.weights = weights
 
         self._n_examples = df.shape[0]
-        self._n_unique = df.index.unique().shape[0]
+        self._n_unique = df.index.nunique()
         return self
 
     def _check_values(self, df, events, start, stop):
