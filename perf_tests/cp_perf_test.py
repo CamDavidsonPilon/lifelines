c7df0c246c803d4af4807266c76f4a5f8ca255d0
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2020-01-23 22:56:52 -0500

    see #935

--- perf_tests/cp_perf_test.py
+++ perf_tests/cp_perf_test.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     from lifelines import CoxPHFitter
     from lifelines.datasets import load_rossi
 
-    reps = 10
+    reps = 1
     df = load_rossi()
     df = pd.concat([df] * reps)
-    cp = CoxPHFitter(penalizer=20 * reps, l1=True)
+    cp = CoxPHFitter(penalizer=0.001, l1=True)
     start_time = time.time()
     cp.fit(df, duration_col="week", event_col="arrest", batch_mode=True)
     print("--- %s seconds ---" % (time.time() - start_time))
